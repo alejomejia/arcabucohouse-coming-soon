3829fd77415a41619eba51ec922fbb7fdd9e8502
Alejandro Mejia <alejomejiacuartas@outlook.com>
2025-12-11 15:22:46 +0100

    add env variables

---
+++ src/utils/const.ts
@@ -0,0 +1,11 @@
+export const SEO_DEFAULT = {
+  title: 'Arcabuco ~ High-end interiorism made by Colombian artisans',
+  description: 'High-end interiorism made by Colombian artisans',
+  imageUrl: 'https://arcabucohouse.com/assets/seo/og-image.webp'
+}
+
+export const config = {
+  DEV: import.meta.env.DEV,
+  UMAMI_WEBSITE_ID: import.meta.env.UMAMI_WEBSITE_ID,
+  WEBSITE_DOMAIN: import.meta.env.WEBSITE_DOMAIN
+}
