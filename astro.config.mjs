70ed1b30a0681d8b84f38712a250f0273e4178cc
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-07-01 22:43:19 +0200

    add domain to allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   site: 'https://arcabucohouse.com',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    preview: {
+      allowedHosts: ["arcabucohouse.com", "www.arcabucohouse.com"],
+    },
   }
 })
