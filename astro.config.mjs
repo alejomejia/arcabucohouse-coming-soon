c038a637114bd4ad4047134c97f4edcb9367f6e6
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-07-01 23:25:45 +0200

    add removed tailwindcss vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   site: 'https://arcabucohouse.com',
   output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
